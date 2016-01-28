ff91c89fb42a86b64d8d4db7563370c34a569f62
Michael Andrews <mandrews@wpi.edu>
2016-01-27 21:01:42 -0500

    Add schematic prototypes.

--- src/include/schematic.h
+++ src/include/schematic.h
@@ -27,6 +27,7 @@ vec3 vec3_max(vec3 a);
 // vec3 -> int
 int vec3_sqr(vec3 a); // x^2 + y^2 + z^2
 int vec3_dot(vec3 a, vec3 b);
+int vec3_vol(vec3 a);
 
 
 typedef int16_t block_t;
@@ -37,14 +38,29 @@ struct schematic
 
     block_t blocks[];
 };
+typedef struct schematic schem
 
-struct schematic *schem_init(int x, int y, int z);
-struct schematic *schem_copy(struct schematic *schem);
-void schem_kill(struct schematic *schem);
+schem *schem_init(int x, int y, int z);
+schem *schem_copy(schem *schem);
+void schem_kill(schem *schem);
 
-struct schematic *schem_from_file(const char *path);
-int schem_to_file(struct schematic *schem);
+vec3 schem_size(schem *schem);
+int schem_volume(schem *schem);
 
-block_t schem_get(struct schematic *schem, int 
+
+schem *schem_from_file(const char *path);
+int schem_to_file(schem *schem);
+
+block_t schem_get(schem *schem, vec3 coord);
+int schem_set(schem *schem, vec3 coord, block_t block);
+
+int schem_resize(schem *schem, vec3 size);
+int schem_shift(schem *schem, vec3 offset);
+int schem_stack(schem *schem, vec3 counts);
+int schem_flip(schem *schem, vec3 dirs); // Flips n times on each axis.
+int schem_rotate(schem *schem, vec3 dirs); // Rotates by pi/2 n times on each axis.
+int schem_fill(schem *schem, vec3 offset, vec3 size, block_t block);
+int schem_insert(schem *into, vec3 offset, schem *from);
+int schem_stacking_resize(schem *schem, vec3 size);
 
 #endif // SCHEMATIC_H
